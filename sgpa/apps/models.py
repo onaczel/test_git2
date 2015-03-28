6cb07c264c6a6e17203286ee217cd1c99ffb1ad6
selm <selm@outlook.com>
2015-03-28 05:30:19 -0400

    Se añadieron docstrings

--- sgpa/apps/models.py
+++ sgpa/apps/models.py
@@ -7,24 +7,36 @@ from test.test_support import args_from_interpreter_flags
 
     
 class Roles(models.Model):
+    """
+    Modelo para almacenar los roles posibles.
+    """
     descripcion = models.CharField(max_length=200, unique=True)
     estado = models.BooleanField(default = 1)
     def __str__(self):
         return self.descripcion
 
 class Users_Roles(models.Model):
+    """
+    Modelo que asocia un usuario con un rol
+    """
     user = models.ForeignKey(User)
     role = models.ForeignKey(Roles)
     def __str__(self):
         return self.role
 
 class Permisos(models.Model):
+    """
+    Modelo para almacenar los permisos
+    """
     descripcion = models.CharField(max_length=50)
     tag = models.CharField(max_length=50)
     def __str__(self):
         return self.descripcion
 
 class Permisos_Roles(models.Model):
+    """
+    Modelo que asocia un rol con un permiso
+    """
     roles = models.ForeignKey(Roles)
     permisos = models.ForeignKey(Permisos)
 
