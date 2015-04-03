a621308513512064719258116bfa32fb7278abce
selm <selm@outlook.com>
2015-04-02 22:08:55 -0400

    elimine algunos migrate viejos

--- sgpa/apps/migrations/0001_initial.py
+++ sgpa/apps/migrations/0001_initial.py
@@ -13,11 +13,65 @@ class Migration(migrations.Migration):
 
     operations = [
         migrations.CreateModel(
+            name='Actividades',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('descripcion', models.CharField(max_length=50)),
+                ('estado', models.BooleanField(default=True)),
+                ('plantilla', models.BooleanField(default=True)),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='Actividades_Estados',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('actividad', models.ForeignKey(to='apps.Actividades')),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='Equipo',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='Estados',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('descripcion', models.CharField(max_length=50)),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='Flujos',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('descripcion', models.CharField(max_length=50)),
+                ('pantilla', models.BooleanField(default=True)),
+                ('estado', models.BooleanField(default=True)),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
             name='Permisos',
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
                 ('descripcion', models.CharField(max_length=50)),
                 ('tag', models.CharField(max_length=50)),
+                ('estado', models.BooleanField(default=False)),
             ],
             options={
             },
@@ -34,10 +88,24 @@ class Migration(migrations.Migration):
             bases=(models.Model,),
         ),
         migrations.CreateModel(
+            name='Proyectos',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('nombre', models.CharField(max_length=50)),
+                ('fecha_ini', models.DateField()),
+                ('fecha_est_fin', models.DateField()),
+                ('descripcion', models.CharField(max_length=400)),
+                ('observaciones', models.CharField(max_length=400)),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
             name='Roles',
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-                ('descripcion', models.CharField(max_length=200)),
+                ('descripcion', models.CharField(unique=True, max_length=200)),
                 ('estado', models.BooleanField(default=1)),
             ],
             options={
@@ -59,6 +127,42 @@ class Migration(migrations.Migration):
             model_name='permisos_roles',
             name='roles',
             field=models.ForeignKey(to='apps.Roles'),
+            preserve_default=True,
+        ),
+        migrations.AddField(
+            model_name='flujos',
+            name='proyeto',
+            field=models.ForeignKey(blank=True, to='apps.Proyectos', null=True),
+            preserve_default=True,
+        ),
+        migrations.AddField(
+            model_name='equipo',
+            name='proyecto',
+            field=models.ForeignKey(to='apps.Proyectos'),
+            preserve_default=True,
+        ),
+        migrations.AddField(
+            model_name='equipo',
+            name='rol',
+            field=models.ForeignKey(to='apps.Roles'),
+            preserve_default=True,
+        ),
+        migrations.AddField(
+            model_name='equipo',
+            name='usuario',
+            field=models.ForeignKey(to=settings.AUTH_USER_MODEL),
+            preserve_default=True,
+        ),
+        migrations.AddField(
+            model_name='actividades_estados',
+            name='estados',
+            field=models.ForeignKey(to='apps.Estados'),
+            preserve_default=True,
+        ),
+        migrations.AddField(
+            model_name='actividades',
+            name='flujo',
+            field=models.ForeignKey(to='apps.Flujos'),
             preserve_default=True,
         ),
     ]
