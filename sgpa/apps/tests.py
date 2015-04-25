71d8b141119d86c5384d5843cb6977f582b35b50
Ariel Díaz <lastdeo@gmail.com>
2015-04-25 00:16:53 -0400

    recorregidas las pruebas unitarias de Ariel

--- sgpa/apps/tests.py
+++ sgpa/apps/tests.py
@@ -650,10 +650,12 @@ class test_sprint(TestCase):
         proyecto.nro_sprint = 1
         proyecto.save()
         
-        sprint = Sprint()
-        sprint.proyecto_id = proyecto.id
-        sprint.nro_sprint = proyecto.nro_sprint
-        sprint.save()
+        crearSprints(proyecto.id)
+        sprint = Sprint.objects.get(proyecto_id = proyecto.id, nro_sprint = 1)
+        #sprint = Sprint()
+        #sprint.proyecto_id = proyecto.id
+        #sprint.nro_sprint = proyecto.nro_sprint
+        #sprint.save()
         
         us = UserStory()
         us.descripcion = 'test user story'
@@ -663,14 +665,14 @@ class test_sprint(TestCase):
         us.sprint = sprint.id
         us.save()
         
-        dia_sprint = Dia_Sprint()
-        dia_sprint.fecha = datetime.today().strftime("%Y-%m-%d")
-        dia_sprint.sprint_id = us.sprint
-        dia_sprint.tiempo_estimado = 0
-        dia_sprint.tiempo_real = 0
-        dia_sprint.save()
+        #dia_sprint = Dia_Sprint()
+        #dia_sprint.fecha = datetime.today().strftime("%Y-%m-%d")
+        #dia_sprint.sprint_id = us.sprint
+        #dia_sprint.tiempo_estimado = 0
+        #dia_sprint.tiempo_real = 0
+        #dia_sprint.save()
         
-        self.assertTrue(Dia_Sprint.objects.filter(fecha = datetime.today().strftime("%Y-%m-%d"), sprint_id = us.sprint).exists(), "No se crearon los dias del sprint")
+        self.assertTrue(Dia_Sprint.objects.get(fecha = datetime.today().strftime("%Y-%m-%d"), sprint_id = us.sprint).exists(), "No se crearon los dias del sprint")
         
     def test_agregar_horas(self):
         """
@@ -686,10 +688,12 @@ class test_sprint(TestCase):
         proyecto.nro_sprint = 1
         proyecto.save()
         
-        sp = Sprint()
-        sp.proyecto_id = proyecto.id
-        sp.nro_sprint = proyecto.nro_sprint
-        sp.save()
+        crearSprints(proyecto.id)
+        sprint = Sprint.objects.get(proyecto_id = proyecto.id, nro_sprint = 1)
+        #sp = Sprint()
+        #sp.proyecto_id = proyecto.id
+        #sp.nro_sprint = proyecto.nro_sprint
+        #sp.save()
         
         
         us = UserStory()
@@ -697,15 +701,16 @@ class test_sprint(TestCase):
         us.codigo = 'us1_p1'
         us.tiempo_Estimado = 50
         us.proyecto_id = proyecto.id
-        us.sprint = sp.id
+        us.sprint = sprint.id
         us.save()
         
-        dia_sprint = Dia_Sprint()
-        dia_sprint.fecha = datetime.today().strftime("%Y-%m-%d")
-        dia_sprint.sprint_id = us.sprint
-        dia_sprint.tiempo_estimado = 0
-        dia_sprint.tiempo_real = 0
-        dia_sprint.save()
+        #dia_sprint = Dia_Sprint()
+        #dia_sprint.fecha = datetime.today().strftime("%Y-%m-%d")
+        #dia_sprint.sprint_id = us.sprint
+        #dia_sprint.tiempo_estimado = 0
+        #dia_sprint.tiempo_real = 0
+        #dia_sprint.save()
+        dia_sprint = Dia_Sprint.objects.get(fecha = datetime.today().strftime("%Y-%m-%d"), sprint_id = us.sprint)
         
         dia_sprint.tiempo_real = int(dia_sprint.tiempo_real) + 10
         dia_sprint.save()
