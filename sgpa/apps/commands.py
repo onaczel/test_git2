86e23a61a4bd4d866a7d58405127512efd0b939e
selm <selm@outlook.com>
2015-05-16 04:33:37 -0400

    arregle la funcion

--- sgpa/apps/commands.py
+++ sgpa/apps/commands.py
@@ -57,7 +57,7 @@ def notificarModificacionHU(hu_id, proyecto_id):
             list = []
             list.append(user)
             asunto = 'SGPA - Modificacion de User Story'
-            msg = 'Usuario :'+user.username+', se ha modificado el  user story: '+hu.nombre+' del proyecto: '+proyecto.nombre
+            msg = 'Usuario: '+user.username+', se ha modificado el  user story: '+hu.nombre+' del proyecto: '+proyecto.nombre
             enviarMail(asunto, msg, list)
 
 def notificarCambioResponsableHU(new_id, old_id,hu_id, proyecto_id):
@@ -66,37 +66,43 @@ def notificarCambioResponsableHU(new_id, old_id,hu_id, proyecto_id):
     @param proyecto_id: id de un proyecto
     @param hu_id: id de un user story
     """
-    
+    print'old'
+    print old_id
+    print'new'
+    print new_id
     historial = historialResponsableHU.objects.filter(hu = hu_id)
     hu = UserStory.objects.get(id = hu_id)
     proyecto = Proyectos.objects.get(id = proyecto_id)
+    band = True
+    try: 
+        Unew = User.objects.get(id = new_id)
+        list = []
+        list.append(Unew)
+        asunto = 'SGPA - Asignacion a User Story'
+        msg = 'Usuario: '+Unew.username+', se le ha asignado como responsable del user story: '+hu.nombre+' del proyecto: '+proyecto.nombre
+        enviarMail(asunto, msg, list)
+        band = True
     
-    #try: 
-    Unew = User.objects.get(id = new_id)
-    list = []
-    list.append(Unew)
-    asunto = 'SGPA - Asignacion a User Story'
-    msg = 'Usuario :'+Unew.username+', se le ha asignado como responsable del user story: '+hu.nombre+' del proyecto: '+proyecto.nombre
-    enviarMail(asunto, msg, list)
-    '''''
     except:
         Uold = User.objects.get(id = old_id)
         list = []
         list.append(Uold)
         asunto = 'SGPA - Asignacion a User Story'
-        msg = 'Usuario :'+Uold.username+', se le ha asignado como responsable del user story: '+hu.nombre+' del proyecto: '+proyecto.nombre
-        enviarMail(asunto, msg, list)
-    '''
-    try:
-        Uold = User.objects.get(id = old_id)
-        l = []
-        l.append(Uold)
-        asunto = 'SGPA - Desasignacion de User Story'
-        msg = 'Usuario :'+Unew.username+', ha desasignado como responsable del user story: '+hu.nombre+' del proyecto: '+proyecto.nombre
-        enviarMail(asunto, msg, l)
-   
-    except:
-        pass
+        msg3 = 'Usuario: '+Uold.username+', se le ha asignado como responsable del user story: '+hu.nombre+' del proyecto: '+proyecto.nombre
+        enviarMail(asunto, msg3, list)
+        band = False
+    
+    if band == True:
+        try:
+            Uold = User.objects.get(id = old_id)
+            l = []
+            l.append(Uold)
+            asunto = 'SGPA - Desasignacion de User Story'
+            msg2 = 'Usuario: '+Unew.username+', ha sido desasignado como responsable del user story: '+hu.nombre+' del proyecto: '+proyecto.nombre
+            enviarMail(asunto, msg2, l)
+       
+        except:
+            pass
 
 def notificarRegistroTrabajo(hu_id, proyecto_id):
     """
