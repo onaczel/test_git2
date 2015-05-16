4f0a0126f1052b02b05d1b41c7d260685e159af2
selm <selm@outlook.com>
2015-05-15 20:54:59 -0400

    nuevas funciones

--- sgpa/apps/commands.py
+++ sgpa/apps/commands.py
@@ -5,7 +5,7 @@ from django.core.mail import send_mail
 
 from djutils.decorators import async
 
-from apps.models import UserStory, Proyectos, historialResponsableHU
+from apps.models import UserStory, Proyectos, historialResponsableHU, Equipo
 from django.contrib.auth.models import User
 
 @async
@@ -47,9 +47,18 @@ def notificarModificacionHU(hu_id, proyecto_id):
             enviarMail(asunto, msg, list)
 
 
-#def notificarRegistroTrabajo(hu_id):
-            
-            
+def notificarRegistroTrabajo(hu_id, proyecto_id):
+    
+    proyecto = Proyectos.objects.get(id = proyecto_id)
+    equipo = Equipo.objects.get(proyecto_id = proyecto_id, rol_id = 3)
+    hu = UserStory.objects.get(id = hu_id)
+    scrumMaster = User.objects.get(id = equipo.usuario_id)
+    
+    list = []
+    list.append(scrumMaster)
+    asunto = 'SGPA - Registro de trabajo'
+    msg = 'Scrum Master :'+scrumMaster.username+', se ha registrado trabajo en el user story: '+hu.nombre+' del proyecto: '+proyecto.nombre
+    enviarMail(asunto, msg, list)    
           
             
             
