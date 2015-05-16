f463427d85e859ba30c13b789c86131a7fe16fea
selm <selm@outlook.com>
2015-05-16 04:59:39 -0400

    corregi la notificacion la desasignacion y asignacion

--- sgpa/apps/commands.py
+++ sgpa/apps/commands.py
@@ -60,7 +60,7 @@ def notificarModificacionHU(hu_id, proyecto_id):
             msg = 'Usuario: '+user.username+', se ha modificado el  user story: '+hu.nombre+' del proyecto: '+proyecto.nombre
             enviarMail(asunto, msg, list)
 
-def notificarCambioResponsableHU(new_id, old_id,hu_id, proyecto_id):
+def notificarCambioResponsableHU(old_id, new_id,hu_id, proyecto_id):
     """
     Prepara un email de notificacion de cuando de modifica un  user story
     @param proyecto_id: id de un proyecto
@@ -81,7 +81,7 @@ def notificarCambioResponsableHU(new_id, old_id,hu_id, proyecto_id):
         asunto = 'SGPA - Asignacion a User Story'
         msg = 'Usuario: '+Unew.username+', se le ha asignado como responsable del user story: '+hu.nombre+' del proyecto: '+proyecto.nombre
         enviarMail(asunto, msg, list)
-        band = True
+        
     
     except:
         Uold = User.objects.get(id = old_id)
@@ -98,7 +98,7 @@ def notificarCambioResponsableHU(new_id, old_id,hu_id, proyecto_id):
             l = []
             l.append(Uold)
             asunto = 'SGPA - Desasignacion de User Story'
-            msg2 = 'Usuario: '+Unew.username+', ha sido desasignado como responsable del user story: '+hu.nombre+' del proyecto: '+proyecto.nombre
+            msg2 = 'Usuario: '+Uold.username+', ha sido desasignado como responsable del user story: '+hu.nombre+' del proyecto: '+proyecto.nombre
             enviarMail(asunto, msg2, l)
        
         except:
